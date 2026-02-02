a3bab66da34b25e77013c4ba243e94d78be55d74
jm <jm@synergy.local>
2026-02-02 19:08:23 -0300

    cambio la politica SD POC andando

--- boiola-tool.ino
+++ boiola-tool.ino
@@ -1,10 +1,5 @@
-// #include <SPI.h>
-// #include <XPT2046_Touchscreen.h>
-// #include <TFT_eSPI.h>
 #include <HardwareSerial.h>
-#include "FS.h"
-#include "SD.h"
-// #include <XPT2046_Bitbang.h>
+#include <SD.h>
 
 #define TOUCH_CLK 25
 #define TOUCH_MOSI 32
@@ -28,24 +23,45 @@ HardwareSerial gpsSerial(1);
 bool estado_pantalla = true;
 bool estado_sd = false;
 
+String dataBuffer;
+File myFile;
+const char filename[] = "/foo.txt";
+
 void setup() {
-  SPIClass spi = SPIClass(VSPI);
+  // SPIClass spi = SPIClass(VSPI);
   pantalla_init(tft);  // inicia la pantalla
 
+  pantalla_bandera(tft, 30, 30, 5);
+  pantalla_bandera(tft, 50, 50, 5);
+  pantalla_bandera(tft, 70, 70, 5);
+  pantalla_bandera(tft, 0, 0, 5);
+
+  delay(1000);
   pantalla_setup(tft, TFT_PURPLE);
 
   pantalla_bandera(tft, 0, 0, 5);
 
   touch.begin();  // inicia el touch
 
-  estado_sd = SD.begin(SS, spi, 80000000);
-
-  if (estado_sd) {
+  if (SD.begin(SS)) {
     tft.drawBitmap(208, 0, sd_icon, 32, 32, TFT_BLACK, TFT_GREEN);
   } else {
     tft.drawBitmap(208, 0, sd_icon, 32, 32, TFT_BLACK, TFT_RED);
   }
   gpsSerial.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+
+
+  dataBuffer.reserve(1024);
+  myFile = SD.open(filename, FILE_WRITE);
+  if (!myFile) {
+    tft.setCursor(10, 100);
+    tft.println("error opening: ");
+    tft.println(filename);
+    while (true);
+  }
+  myFile.println();
+  myFile.println("Hello World!");
+  myFile.close();
 }
 
 
@@ -58,8 +74,9 @@ void loop() {
   if (gps.location.isUpdated()) {
     pantalla_fecha(tft, gps);
     pantalla_gps(tft, gps, 60);
-    char buf[64];
 
+    myFile = SD.open(filename, FILE_APPEND);
+    char buf[64];
     snprintf(buf, sizeof(buf),
              "%02d:%02d:%02d,%.6f,%.6f\n",
              gps.time.hour(),
@@ -67,7 +84,8 @@ void loop() {
              gps.time.second(),
              gps.location.lat(),
              gps.location.lng());
-    appendFile(SD, "/gps.gpx", buf);
+    myFile.print(buf);
+    myFile.close();
   }
 
   pantalla_touch(tft, touch);
@@ -75,34 +93,34 @@ void loop() {
 
 
 
-void writeFile(fs::FS &fs, const char *path, const char *message) {
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file) {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if (file.print(message)) {
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-void appendFile(fs::FS &fs, const char *path, const char *message) {
-  // Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if (!file) {
-    // Serial.println("Failed to open file for appending");
-    return;
-  }
-  if (file.print(message)) {
-    // Serial.println("Message appended");
-  } else {
-    // Serial.println("Append failed");
-  }
-  file.close();
-}
+// void writeFile(fs::FS &fs, const char *path, const char *message) {
+//   Serial.printf("Writing file: %s\n", path);
+//
+//   File file = fs.open(path, FILE_WRITE);
+//   if (!file) {
+//     Serial.println("Failed to open file for writing");
+//     return;
+//   }
+//   if (file.print(message)) {
+//     Serial.println("File written");
+//   } else {
+//     Serial.println("Write failed");
+//   }
+//   file.close();
+// }
+//
+// void appendFile(fs::FS &fs, const char *path, const char *message) {
+//   // Serial.printf("Appending to file: %s\n", path);
+//
+//   File file = fs.open(path, FILE_APPEND);
+//   if (!file) {
+//     // Serial.println("Failed to open file for appending");
+//     return;
+//   }
+//   if (file.print(message)) {
+//     // Serial.println("Message appended");
+//   } else {
+//     // Serial.println("Append failed");
+//   }
+//   file.close();
+// }
